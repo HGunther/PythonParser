bb073eb83935f890bc6ea1a95dc74cb70bf33dba
HGunther <hansgunther@aggiemail.usu.edu>
2016-04-18 13:02:39 -0600

    Added Keywords dictionary with keywords, operators, and delimiters.

---
+++ lexicalAnalyzer.py
@@ -0,0 +1,12 @@
+import sys
+import re
+
+
+if __name__ == "__main__":
+    # read filename from commandline argument
+        # sys.argv is a list of command line arguments
+    # read in each line
+        # tokenize with magnificant regex
+        # determine the type of each token
+        # print type and token
+    pass
