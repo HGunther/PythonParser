6b133b21b038740abfe73f83cf25f9bbe9ee4d9d
HGunther <hansgunther@aggiemail.usu.edu>
2016-04-18 13:06:15 -0600

    Added keywords dictionary containing keywords, operators and delimiters.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PythonParser
 Project for CS4700 Programming Languages
+
+Students:
+Hans Gunther A01077132
--- lexicalAnalyzer.py
+++ lexicalAnalyzer.py
@@ -1,12 +1,39 @@
 import sys
 import re
 
+keywords = {"False": "KEYWORD", "class": "KEYWORD", "finally": "KEYWORD",
+"is": "KEYWORD", "return": "KEYWORD", "None": "KEYWORD", "continue": "KEYWORD",
+"for": "KEYWORD", "lambda": "KEYWORD", "try": "KEYWORD", "True": "KEYWORD",
+"def": "KEYWORD", "from": "KEYWORD", "nonlocal": "KEYWORD", "while": "KEYWORD",
+"and": "KEYWORD", "del": "KEYWORD", "global": "KEYWORD", "not": "KEYWORD",
+"with": "KEYWORD", "as": "KEYWORD", "elif": "KEYWORD", "if": "KEYWORD",
+"or": "KEYWORD", "yield": "KEYWORD", "assert": "KEYWORD", "else": "KEYWORD",
+"import": "KEYWORD", "pass": "KEYWORD", "break": "KEYWORD", "except": "KEYWORD",
+"in": "KEYWORD", "raise": "KEYWORD",
+
+"+": "OPERATOR", "-": "OPERATOR", "*": "OPERATOR",  "**": "OPERATOR", "/": "OPERATOR",
+"//": "OPERATOR", "%": "OPERATOR", "@": "OPERATOR", "<<": "OPERATOR", ">>": "OPERATOR", 
+"&": "OPERATOR", "|": "OPERATOR", "^": "OPERATOR", "~": "OPERATOR", "<": "OPERATOR", 
+">": "OPERATOR", "<=": "OPERATOR", ">=": "OPERATOR", "==": "OPERATOR", "!=": "OPERATOR", 
+
+"(": "DELIMITER", ")": "DELIMITER", "[": "DELIMITER", "]": "DELIMITER",
+"{": "DELIMITER", "}": "DELIMITER", ",": "DELIMITER", ":": "DELIMITER",
+".": "DELIMITER", ";": "DELIMITER", "@": "DELIMITER", "=": "DELIMITER",
+"->": "DELIMITER", "+=": "DELIMITER", "-=": "DELIMITER", "*=": "DELIMITER",
+"/=": "DELIMITER", "//=": "DELIMITER", "%=": "DELIMITER", "@=": "DELIMITER",
+"&=": "DELIMITER", "|=": "DELIMITER", "^=": "DELIMITER", ">>=": "DELIMITER",
+"<<=": "DELIMITER", "**=": "DELIMITER",
+
+"$": "ERROR: symbol not used in python",
+"?": "ERROR: symbol not used in python",
+"`": "ERROR: symbol not used in python",
+}
+
+def isKeyword(string):
+    return keywords.has_key(string)
 
 if __name__ == "__main__":
-    # read filename from commandline argument
-        # sys.argv is a list of command line arguments
-    # read in each line
-        # tokenize with magnificant regex
-        # determine the type of each token
-        # print type and token
-    pass
+    inFile = open("testInput.py", 'r') # opens file as read-only
+    for line in inFile:
+        pass
+    inFile.close()
